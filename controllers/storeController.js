c638642d39b26ae4d48b66c3884625a29044b050
Chris Condreay <condreaychris@gmail.com>
2025-06-24 07:51:04 -0700

    setting up stores and adding routes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     res.render('index');
 };
 
 exports.addStore = (req, res) => {
     res.render('editStore', {title: 'Add Store'})
+};
+
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}, please leave us a feedback!`);
+    res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+    
+
+    res.render('stores', { title: 'Stores'})
 }
--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -1,3 +1,9 @@
+exports.catchErrors = (fn) => {
+    return function(req, res, next) {
+        return fn(req, res, next).catch(next);
+    }
+}
+
 exports.notFound = (req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
