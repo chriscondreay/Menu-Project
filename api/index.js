e7291c83ace2b22aca4a2cd2a796d2ac0f4e60f4
Chris Condreay <condreaychris@gmail.com>
2025-08-06 03:09:44 -0700

    more configuration

--- api/index.js
+++ api/index.js
@@ -1,4 +1,21 @@
+const mongoose = require('mongoose');
 const serverless = require('serverless-http');
-const app = require('../app');
+
+require('dotenv').config({ path: '.env' });
+
+// mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+    console.error(`${err.message}`);
+});
+
+require('./models/Store');
+require('./models/User');
+
+const app = require('./app');
+app.set('port', process.env.PORT || 8800);
+// const server = app.listen(app.get('port'), () => {
+//     console.log(`Express running -> PORT ${server.address().port}`)
+// });
 
 module.exports = serverless(app);
--- start.js
+++ start.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// const serverless = require('serverless-http');
 
-require('dotenv').config({ path: '.env' });
+// require('dotenv').config({ path: '.env' });
 
-mongoose.connect(process.env.DATABASE);
-mongoose.Promise = global.Promise;
-mongoose.connection.on('error', (err) => {
-    console.error(`${err.message}`);
-});
+// // mongoose.connect(process.env.DATABASE);
+// mongoose.Promise = global.Promise;
+// mongoose.connection.on('error', (err) => {
+//     console.error(`${err.message}`);
+// });
 
-require('./models/Store');
-require('./models/User');
+// require('./models/Store');
+// require('./models/User');
 
-const app = require('./app');
-app.set('port', process.env.PORT || 8800);
-const server = app.listen(app.get('port'), () => {
-    console.log(`Express running -> PORT ${server.address().port}`)
-});
+// const app = require('./app');
+// app.set('port', process.env.PORT || 8800);
+// // const server = app.listen(app.get('port'), () => {
+// //     console.log(`Express running -> PORT ${server.address().port}`)
+// // });
+
+// module.exports = serverless(app);
